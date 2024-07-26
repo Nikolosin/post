871cdc3f71162307879d0200c8cd88323701e7ec
Nikolay Petrenko <nikolosin73@gmail.com>
2024-07-26 12:23:09 +0300

    add nullable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,14 @@ fun main() {
 }
 
 
-    data class Post(
+data class Post(
     val id: Int,
     val ownerId: Int,
     val fromId: Int,
     val createdBy: Int,
-    val text: String,
+    val text: String?,
     val friendsOnly: Boolean = false,
-    val likes: Likes = Likes(0, false, true, true),
+    val likes: Likes?,
     val postType: String,
     val signerId: Int,
     val canDelete: Boolean = true
@@ -30,14 +30,14 @@ object WallService {
     private var lastId = 0
 
     fun add(post: Post): Post {
-        posts += post.copy(id = ++lastId, likes = post.likes.copy())
+        posts += post.copy(id = ++lastId, likes = post.likes?.copy())
         return posts.last()
     }
 
     fun update(newPost: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (post.id == newPost.id) {
-                posts[index] = newPost.copy(likes = post.likes.copy())
+                posts[index] = newPost.copy(likes = post.likes?.copy())
                 return true
             }
         }
