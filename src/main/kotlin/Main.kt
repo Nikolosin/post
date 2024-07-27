5b650c0800c2cc197afa89f4f0fccbc36b2c2a0d
Nikolay Petrenko <nikolosin73@gmail.com>
2024-07-27 12:31:54 +0300

    add PostNotFoundException

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,21 @@
 package ru.netology
 
 fun main() {
+    val com: Comment = Comment(1, 1, "sad", 1)
+    val arrayComments = emptyArray<Comment>()
+    val arrayAttachment = arrayOf(
+        FileAttachment(File(2, 2, "name", 128)),
+        VideoAttachment(Video(1, 2, "game", 2800)),
+        AudioAttachment(Audio(1, 1, "Qween", "Show"))
+    )
+    val post: Post =
+        Post(2, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false, arrayComments, arrayAttachment)
+    val post2: Post =
+        Post(2, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false, arrayComments, arrayAttachment)
+    val service = WallService
+    println(service.add(post))
+    println(service.add(post2))
+    println(service.createComment(2, com))
 }
 
 
@@ -15,6 +30,7 @@ data class Post(
     val postType: String,
     val signerId: Int,
     val canDelete: Boolean = true,
+    val comments: Array<Comment> = emptyArray(),
     val attachments: Array<Attachment> = emptyArray()
 )
 
@@ -84,30 +100,56 @@ data class Likes(
     val canPublish: Boolean
 )
 
+data class Comment(
+    val id: Int,
+    val ownerId: Int,
+    val text: String,
+    val data: Int
+)
+
+class PostNotFoundException(mes: String) : RuntimeException(mes)
+
 object WallService {
-    private var posts = emptyArray<Post>()
+    var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
     private var lastId = 0
+    private var lastIdComment = 0
 
     fun add(post: Post): Post {
         posts += post.copy(id = ++lastId, likes = post.likes?.copy())
         return posts.last()
     }
 
-    fun update(newPost: Post): Boolean {
+    fun createComment(postId: Int, comment: Comment): Comment {
+        var match = false
         for ((index, post) in posts.withIndex()) {
-            if (post.id == newPost.id) {
-                posts[index] = newPost.copy(likes = post.likes?.copy())
-                return true
+            if (post.id == postId) {
+                match = true
+                comments += comment.copy(id =++lastIdComment)
+                posts[index] = post.copy(comments = comments)
+                break
             }
         }
-        return false
+        if (match) {
+            return comments.last()
+        } else throw PostNotFoundException("no post with id=$postId")
     }
 
-    fun clear() {
-        posts = emptyArray()
-        lastId = 0
+        fun update(newPost: Post): Boolean {
+            for ((index, post) in posts.withIndex()) {
+                if (post.id == newPost.id) {
+                    posts[index] = newPost.copy(likes = post.likes?.copy())
+                    return true
+                }
+            }
+            return false
+        }
+
+        fun clear() {
+            posts = emptyArray()
+            lastId = 0
+        }
     }
-}
 
 
 
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -3,6 +3,7 @@ import org.junit.Before
 
 import org.junit.Test
 import ru.netology.*
+import ru.netology.WallService.posts
 
 class WallServiceTest {
 
@@ -14,16 +15,37 @@ class WallServiceTest {
     @Test
     fun add() {
         val service = WallService
-        val post = service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false))
+        val post =
+            service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false))
         val result = post.id != 0
         assertTrue(result)
-
     }
+
     @Test
     fun update() {
         val service = WallService
-        val arrayAttachment = arrayOf(FileAttachment(File(2,2,"name", 128)), VideoAttachment(Video(1,2,"game", 2800)), AudioAttachment(Audio(1,1,"Qween", "Show")))
-        service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false, arrayAttachment))
+        val arrayAttachment = arrayOf(
+            FileAttachment(File(2, 2, "name", 128)),
+            VideoAttachment(Video(1, 2, "game", 2800)),
+            AudioAttachment(Audio(1, 1, "Qween", "Show"))
+        )
+        val arrayComments = emptyArray<Comment>()
+        service.add(
+            Post(
+                0,
+                1,
+                1,
+                1,
+                "hello",
+                false,
+                likes = Likes(1, false, false, false),
+                "post",
+                1,
+                false,
+                arrayComments,
+                arrayAttachment
+            )
+        )
         service.add(Post(1, 1, 1, 1, "hello world", false, likes = Likes(1, false, false, false), "post", 1, false))
         val update = Post(1, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false)
         val result = service.update(update)
@@ -40,5 +62,30 @@ class WallServiceTest {
         val result = service.update(update)
 
         assertFalse(result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        val service = WallService
+        service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false))
+        service.createComment(2, Comment(1, 1, "Cool", 1))
+    }
+
+    @Test
+    fun addComment() {
+        val comment: Comment = Comment(1, 1, "first", 1)
+        val service = WallService
+        service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false))
+        service.add(Post(0, 1, 1, 1, "hello", false, likes = Likes(1, false, false, false), "post", 1, false))
+        val postId = 1
+        service.createComment(postId, comment)
+        var indexPost = 0
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == postId) {
+                indexPost = index
+            }
+        }
+        assertTrue(posts[indexPost].comments.last() == comment)
+
     }
 }
